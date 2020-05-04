3fc8b62013e93156fbf17324c6c0c897c6979ff7
Fayk Chaudhry <faykc123@gmail.com>
2020-05-04 17:24:32 -0400

    Slight modification

--- 26-RemoveDuplicateSortedArray.py
+++ 26-RemoveDuplicateSortedArray.py
@@ -2,7 +2,9 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums: list) -> int:
+        if len(nums) == 0 or len(nums) == 1:
+            return len(nums)
         originalIndex = 1
         for x in nums[originalIndex::]:
             if originalIndex < len(nums) and x==nums[originalIndex-1]:
